a88a91cf0ef4815cab1200f2a65f9fd13b58feef
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-11-15 17:23:13 +0700

    thuanvv/fix-issue-1511

--- app/Http/Controllers/FirstRegisterMemberController.php
+++ app/Http/Controllers/FirstRegisterMemberController.php
@@ -69,21 +69,6 @@ class FirstRegisterMemberController extends Controller
     }
     public function firstUpdateRegisterMember(Request $request, UtilsCommonHelper $commonController, $memberId)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'vjgr_code' => 'required|string|max:255',
-            'handicap_vga' => 'nullable|string|max:255',
-            'gender' => 'required|int',
-            'date_of_birth' => 'required|date_format:d/m/Y H:i:s|max:255',
-            'nationality' => 'required|string|max:255',
-            'email' => 'nullable|email|unique:member,email,' . $memberId . '|max:255',
-            'phone_number' => 'required|string|max:255',
-            'guardian_name' => 'required|string|max:255',
-            'relationship' => 'required|string|max:255',
-            'guardian_email' => 'required|email|unique:member,email,' . $memberId . '|max:255',
-            'guardian_phone' => 'required|string|max:255',
-        ]);
-
         $member = Register::find($memberId);
 
         if (!$member) {
@@ -91,24 +76,24 @@ class FirstRegisterMemberController extends Controller
             return response()->json($response);
         }
 
-        $member->name = $validatedData['name'];
-        $member->vjgr_code = $validatedData['vjgr_code'];
-        $member->handicap_vga = $validatedData['handicap_vga'];
-        $member->gender = $validatedData['gender'];
-        $member->date_of_birth = Carbon::createFromFormat('d/m/Y H:i:s', $validatedData['date_of_birth'])->format('Y-m-d H:i:s');
-        $member->nationality = $validatedData['nationality'];
-        $member->email = $validatedData['email'];
-        $member->phone_number = $validatedData['phone_number'];
-        $member->guardian_name = $validatedData['guardian_name'];
-        $member->relationship = $validatedData['relationship'];
-        $member->guardian_email = $validatedData['guardian_email'];
-        $member->guardian_phone = $validatedData['guardian_phone'];
+        $member->name = $request->input('name');
+        $member->vjgr_code =$request->input('vjgr_code');
+        $member->handicap_vga =$request->input('handicap_vga');
+        $member->gender =$request->input('gender');
+        $member->date_of_birth = Carbon::createFromFormat('d/m/Y H:i:s',$request->input('date_of_birth'))->format('Y-m-d H:i:s');
+        $member->nationality =$request->input('nationality');
+        $member->email =$request->input('email');
+        $member->phone_number =$request->input('phone_number');
+        $member->guardian_name =$request->input('guardian_name');
+        $member->relationship =$request->input('relationship');
+        $member->guardian_email =$request->input('guardian_email');
+        $member->guardian_phone =$request->input('guardian_phone');
 
         if (isset($validatedData['handicap_vga'])) {
-            $member->handicap_vga = $validatedData['handicap_vga'];
+            $member->handicap_vga =$request->input('handicap_vga');
         }
         if (isset($validatedData['guardian_email'])) {
-            $member->email = $validatedData['guardian_email'];
+            $member->email =$request->input('guardian_email');
         }
 
         $member->save();
